6fa4f3e810db4cd2dea3bbe01c0dd161c7d2a41a
Matthew Mason <mmason@medusamedical.com>
2021-10-03 11:28:01 -0300

    Adding more sounds and improved audio manager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,13 +5,28 @@ using UnityEngine;
 [RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    public AudioClip Music;
-    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip bellSound;
 
-    public AudioSource SourceAudio
-    {
-        get => audioSource;
-    }
+    [SerializeField]
+    private AudioClip[] accentSounds;
+
+    [SerializeField]
+    private float accentSoundTimeoutMin = 10.0f;
+    
+    [SerializeField]
+    private float accentSoundTimeoutMax = 40.0f;
+    
+    [SerializeField]
+    private GameObject AccentAudioSourcePrefab;
+
+    private float accentSoundTimeout;
+
+    [SerializeField]
+    private AudioSource MusicAudioSource;
+
+    private AudioSource defaultAudioSource;
+    private float lastVolume = 0.0f;
 
     public void Awake()
     {
@@ -22,8 +37,23 @@ public class AudioManager : MonoBehaviour
         else
         {
             DontDestroyOnLoad(this);
-            audioSource = GetComponent<AudioSource>();
         }
+
+        if (MusicAudioSource is null)
+        {
+            Debug.LogError($"{nameof(MusicAudioSource)} is null");
+            DestroyImmediate(this);
+        }
+
+        defaultAudioSource = this.GetComponent<AudioSource>();
+
+        if (defaultAudioSource is null)
+        {
+            Debug.LogError($"{nameof(defaultAudioSource)} is null");
+            DestroyImmediate(this);
+        }
+
+        accentSoundTimeout = accentSoundTimeoutMin;
     }
 
     public void Start()
@@ -31,14 +61,71 @@ public class AudioManager : MonoBehaviour
         PlayMusic();
     }
 
+    public void FixedUpdate()
+    {
+        accentSoundTimeout -= Time.fixedDeltaTime;
+
+        if (accentSoundTimeout < 0)
+        {
+            accentSoundTimeout = Random.Range(accentSoundTimeoutMin, accentSoundTimeoutMax);
+            PlayAccent();
+        }
+
+        if (lastVolume != defaultAudioSource.volume)
+        {
+            SetAllAudioSourcesVolumes();
+        }
+
+        lastVolume = defaultAudioSource.volume;
+    }
+
+    private void SetAllAudioSourcesVolumes()
+    {
+        AudioSource[] sources = FindObjectsOfType<AudioSource>();
+        
+        foreach(AudioSource source in sources)
+        {
+            source.volume = defaultAudioSource.volume;
+        }
+    }
+
     public void PlayMusic()
     {
-        SourceAudio.clip = Music;
-        SourceAudio.Play();
+        MusicAudioSource.Play();
     }
 
     public void StopMusic()
     {
-        SourceAudio.Stop();
+        MusicAudioSource.Stop();
     }
+
+    public void PlayAccent()
+    {
+        AudioClip source = accentSounds[Random.Range(0, accentSounds.Length)];
+        PlaySound(source);
+    }
+
+    public void RingBell()
+    {
+        PlaySound(bellSound);
+    }
+
+    private void PlaySound(AudioClip source)
+    {
+        if (defaultAudioSource.mute || defaultAudioSource.volume <= 0.0f)
+        {
+            return;
+        }
+
+        GameObject AccentAudioSource = Instantiate(AccentAudioSourcePrefab, this.transform);
+        AccentAudioSource.transform.localPosition = Vector3.zero;
+        AudioSource accentSource = AccentAudioSource.GetComponent<AudioSource>();
+        accentSource.clip = source;
+        accentSource.volume = defaultAudioSource.volume;
+        accentSource.Play();
+
+        Destroy(AccentAudioSource, source.length + 0.5f);
+    }
+
+    
 }
